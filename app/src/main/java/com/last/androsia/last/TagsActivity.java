ddc3c25fd13ad2dfa12a71de320125ae8c4dc2a4
Samoustique <samuel.philipps@gmail.com>
2017-01-02 22:51:18 +0100

    Create Android Studio project

---
+++ app/src/main/java/com/last/androsia/last/TagsActivity.java
@@ -0,0 +1,13 @@
+package com.last.androsia.last;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TagsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tags);
+    }
+}
